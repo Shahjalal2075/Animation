4bb7d6c02ef7de0ab83809e29087dad7e9fce87a
Shahjalal2075 <shahjalal2075@gmail.com>
2024-01-04 17:25:55 +0600

    2 animation add

---
+++ Test/main.js
@@ -0,0 +1,119 @@
+const animate = document.getElementById('animation');
+
+
+const circle2Img = document.getElementById('circle2-img');
+const circle2 = document.getElementById('circle2');
+const text2 = document.getElementById('bg-text-set-2');
+
+circle2.addEventListener('mouseover', function () {
+    circle2Img.src = 'assets/img bg.png';
+    text2.innerHTML = `<p class="bg-text">In publishing and graphic design, Lorem ipsum is a placeholder text
+commonly</p>`;
+});
+
+circle2.addEventListener('mouseout', function () {
+    circle2Img.src = 'assets/img 1.png';
+    text2.innerHTML = ``;
+});
+
+//
+
+const circle3Img = document.getElementById('circle3-img');
+const circle3 = document.getElementById('circle3');
+const text3 = document.getElementById('bg-text-set-3');
+
+circle3.addEventListener('mouseover', function () {
+    circle3Img.src = 'assets/img bg.png';
+    text3.innerHTML = `<p class="bg-text">In publishing and graphic design, Lorem ipsum is a placeholder text
+commonly</p>`;
+});
+
+circle3.addEventListener('mouseout', function () {
+    circle3Img.src = 'assets/img 2.png';
+    text3.innerHTML = ``;
+});
+
+//
+
+const circle4Img = document.getElementById('circle4-img');
+const circle4 = document.getElementById('circle4');
+const text4 = document.getElementById('bg-text-set-4');
+
+circle4.addEventListener('mouseover', function () {
+    circle4Img.src = 'assets/img bg.png';
+    text4.innerHTML = `<p class="bg-text">In publishing and graphic design, Lorem ipsum is a placeholder text
+commonly</p>`;
+});
+
+circle4.addEventListener('mouseout', function () {
+    circle4Img.src = 'assets/img 3.png';
+    text4.innerHTML = ``;
+});
+
+//
+
+const circle5Img = document.getElementById('circle5-img');
+const circle5 = document.getElementById('circle5');
+const text5 = document.getElementById('bg-text-set-5');
+
+circle5.addEventListener('mouseover', function () {
+    circle5Img.src = 'assets/img bg.png';
+    
+    text5.innerHTML = `<p class="bg-text">In publishing and graphic design, Lorem ipsum is a placeholder text
+commonly</p>`;
+});
+
+circle5.addEventListener('mouseout', function () {
+    circle5Img.src = 'assets/img 4.png';
+    
+    text5.innerHTML = ``;
+});
+
+//
+
+const circle6Img = document.getElementById('circle6-img');
+const circle6 = document.getElementById('circle6');
+const text6 = document.getElementById('bg-text-set-6');
+
+circle6.addEventListener('mouseover', function () {
+    circle6Img.src = 'assets/img bg.png';
+    
+    text6.innerHTML = `<p class="bg-text">In publishing and graphic design, Lorem ipsum is a placeholder text
+commonly</p>`;
+});
+
+circle6.addEventListener('mouseout', function () {
+    circle6Img.src = 'assets/img 5.png';
+    
+    text6.innerHTML = ``;
+});
+
+const circle7 = document.getElementById('circle7');
+const circle7Img = document.getElementById('circle7-img');
+const text7 = document.getElementById('bg-text-set-7');
+
+circle7.addEventListener('mouseover', function () {
+    circle7Img.src = 'assets/img bg.png';
+    text7.innerHTML = `<p class="bg-text">In publishing and graphic design, Lorem ipsum is a placeholder text
+commonly</p>`;
+});
+
+circle7.addEventListener('mouseout', function () {
+    circle7Img.src = 'assets/img 6.png';
+    text7.innerHTML = ``;
+});
+
+const circle8Img = document.getElementById('circle8-img');
+const circle8 = document.getElementById('circle8');
+const text8 = document.getElementById('bg-text-set-8');
+
+circle8.addEventListener('mouseover', function () {
+    circle8Img.src = 'assets/img bg.png';
+    text8.innerHTML = `<p class="bg-text">In publishing and graphic design, Lorem ipsum is a placeholder text
+commonly</p>`;
+});
+
+circle8.addEventListener('mouseout', function () {
+    circle8Img.src = 'assets/img 7.png';
+    text8.innerHTML = ``;
+});
